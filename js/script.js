0340dfb47be8317f02972551677633a1d58df163
nquangbaort <baonq@vaixgroup.com>
2022-04-22 21:08:12 +0700

    update-lanling-page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 (function() {
-    // AOS.init({
-    //     disable: 'mobile'
-    // });
+    //Get the button
+    var scrollToTop = document.getElementById("scrollToTop");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTop.style.display = "block";
+    } else {
+        scrollToTop.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    scrollToTop.onclick = function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
 })()
